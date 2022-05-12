60107cf1b5ee54c1f8e2ff6f5fe6aef52ada053a
Robert <roberto.roeschmann.ruiz@gmail.com>
2022-05-12 15:07:22 -0400

    Catch all ByteBufferException based exceptions in ReadNoThrow.

--- src/server/game/Server/Packets/AuthenticationPackets.cpp
+++ src/server/game/Server/Packets/AuthenticationPackets.cpp
@@ -30,7 +30,7 @@ bool WorldPackets::Auth::EarlyProcessClientPacket::ReadNoThrow()
         Read();
         return true;
     }
-    catch (ByteBufferPositionException const& /*ex*/)
+    catch (ByteBufferException const& /*ex*/)
     {
     }
 
