0425f5f80052e42a284ded953b242d2cb985a453
Robert <roberto.roeschmann.ruiz@gmail.com>
2022-06-24 09:37:50 -0400

    Fix stage 7 completion of Broken Shore intro scenario for slow peeps.

--- src/server/newscripts/Scenario/BrokenIslands/broken_islands.cpp
+++ src/server/newscripts/Scenario/BrokenIslands/broken_islands.cpp
@@ -2332,8 +2332,7 @@ public:
 
                 }
 
-                if (me->IsWithinDist(who, 160.0f))
-                    p->UpdateAchievementCriteria(CRITERIA_TYPE_SCRIPT_EVENT_2, 50027);
+                script->DoUpdateAchievementCriteria(CRITERIA_TYPE_SCRIPT_EVENT_2, 50027);
 
                 step7_intro = true;
 
